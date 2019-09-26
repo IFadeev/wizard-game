d6a194ac1440792651ad66bf577b3ba8c84eec4d
Fadeev Ivan <githubfadeev@gmail.com>
2019-09-26 22:10:35 +0300

    Random generate a wizard

--- js/main.js
+++ js/main.js
@@ -10,29 +10,43 @@ window.addEventListener('load', function() {
   let wizardsList = this.document.querySelector('#similar-wizard-template')
   .content.querySelector('.setup-similar-item');
 
+  const wizardsName = ['Альфред','Хуан', 'Мария', 'Кристофер',
+  'Ульрих', 'Йенс', 'Люпин', 'Генрих']; 
+  const wizardsSurname = ['Себастьян', 'Мюллер', 'Шмидт', 
+  'Фишер', 'Вальц', 'Кляйн', 'Шульц'];
+  const wizardsCoatColor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 
+  'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)',
+  'rgb(0, 0, 0)', 'rgb(255, 255, 255)'];
+  const wizardsEyesColor = ['black', 'red', 'blue', 'brown', 'green'];
+
+  const generateRandom = arr => {
+    return Math.floor(Math.random()*arr.length);
+  }
+
+  const generateWiard = () => {
+    return wizard = {
+      name: wizardsName[generateRandom(wizardsName)],
+      surname: wizardsSurname[generateRandom(wizardsSurname)],
+      coatColor: wizardsCoatColor[generateRandom(wizardsCoatColor)],
+      eyesColor: wizardsEyesColor[generateRandom(wizardsEyesColor)]
+    }
+  }
+
   for (let i = 0; i < 4; i++) {
     let wizardElement = wizardsList.cloneNode(true); 
     
+    wizardElement.querySelector('.setup-similar-label')
+    .textContent = `${generateWiard().name} ${generateWiard().surname}`;
+
+    wizardElement.querySelector('.wizard-coat')
+    .style.fill = generateWiard().coatColor;
+
+    wizardElement.querySelector('.wizard-eyes')
+    .style.fill = generateWiard().eyesColor; 
+    
     elements.appendChild(wizardElement);
-  }
 
-
-  let wizarads = [
-    {
-      name: 'Сарумян',
-      coatColor: 'red',
-  },
-  {
-    name: 'Пендаль Белый',
-    coatColor: 'white'
-  }, 
-  {
-    name: 'Дамболдор',
-    coatColor: 'blue'
-  }
-  ];
-
-
+  };
 
 
 
