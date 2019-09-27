39790d8f235d8f1101fe45c4828afd1c424f604e
Fadeev Ivan <githubfadeev@gmail.com>
2019-09-27 09:47:25 +0300

    Created function for wizard render in dom

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ window.addEventListener('load', function() {
   let userDialog = this.document.querySelector('.setup');
   userDialog.classList.remove('hidden');
 
+  const maxWirads = 4;
+
   document.querySelector('.setup-similar')
   .classList.remove('hidden');
 
@@ -10,32 +12,48 @@ window.addEventListener('load', function() {
   let wizardsList = this.document.querySelector('#similar-wizard-template')
   .content.querySelector('.setup-similar-item');
 
-  for (let i = 0; i < 4; i++) {
-    let wizardElement = wizardsList.cloneNode(true); 
-    
-    elements.appendChild(wizardElement);
+  const wizardsName = ['Альфред','Хуан', 'Мария', 'Кристофер',
+  'Ульрих', 'Йенс', 'Люпин', 'Генрих']; 
+  const wizardsSurname = ['Себастьян', 'Мюллер', 'Шмидт', 
+  'Фишер', 'Вальц', 'Кляйн', 'Шульц'];
+  const wizardsCoatColor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 
+  'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)',
+  'rgb(0, 0, 0)', 'rgb(255, 255, 255)'];
+  const wizardsEyesColor = ['black', 'red', 'blue', 'brown', 'green'];
+
+  const generateRandom = arr => {
+    return Math.floor(Math.random()*arr.length);
   }
 
-
-  let wizarads = [
-    {
-      name: 'Сарумян',
-      coatColor: 'red',
-  },
-  {
-    name: 'Пендаль Белый',
-    coatColor: 'white'
-  }, 
-  {
-    name: 'Дамболдор',
-    coatColor: 'blue'
+  const generateWiard = () => {
+    return wizard = {
+      name: wizardsName[generateRandom(wizardsName)],
+      surname: wizardsSurname[generateRandom(wizardsSurname)],
+      coatColor: wizardsCoatColor[generateRandom(wizardsCoatColor)],
+      eyesColor: wizardsEyesColor[generateRandom(wizardsEyesColor)]
+    }
   }
-  ];
 
+  const renderWizard = (temmplate) => {
+    let wizardElement = temmplate.cloneNode(true);
+    wizardElement.querySelector('.setup-similar-label')
+    .textContent = `${generateWiard().name} ${generateWiard().surname}`;
 
+    wizardElement.querySelector('.wizard-coat')
+    .style.fill = generateWiard().coatColor;
 
+    wizardElement.querySelector('.wizard-eyes')
+    .style.fill = generateWiard().eyesColor;
 
+    return wizardElement
+  }
+
+  const renderDOM = (wizardsList, maxWirads) => {
+    for (let i = 0; i < maxWirads; i++) {
+      elements.appendChild(renderWizard(wizardsList));
+    }
+  }
+
+  renderDOM(wizardsList, maxWirads);  
 
 });
-
-
