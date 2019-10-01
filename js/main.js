f905f7e6c1d661867feb80807d199678e0c03cf1
Fadeev Ivan <githubfadeev@gmail.com>
2019-10-01 21:11:38 +0300

    Moving setting window on move mouse

--- js/main.js
+++ js/main.js
@@ -3,8 +3,9 @@ window.addEventListener('load', function() {
   const setupOpen = this.document.querySelector('.setup-open');
   const setupClose = this.document.querySelector('.setup-close');
   const userSettings = this.document.querySelector('.setup');
-  const userNameInput = this.document.querySelector('.etup-user-name');
-
+  const userNameInput = this.document.querySelector('.setup-user-name');
+  const dialogHendler = this.document.querySelector('.setup-user-pic');
+  
   const openPopup = () => {
     userSettings.classList.remove('hidden');
     document.addEventListener('keydown', onPopupEscPress);
@@ -41,7 +42,44 @@ window.addEventListener('load', function() {
   
   
 
+  dialogHendler.addEventListener('mousedown', function(evt) {
+    evt.preventDefault();
+    
+    let startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    this.document.addEventListener('mousedown', function(evt) {
+      evt.preventDefault();
   
+      let shift = {
+        x: startCoords.x - evt.clientX,
+        y: startCoords.y - evt.clientY
+      };
+
+      startCoords = {
+        x: evt.clientX,
+        y: evt.clientY
+      }; 
+
+      setupClose.style.top
+    });
+    this.document.addEventListener('mouseup', onMouseUp);
+  
+    const onMouseMove = evt => {
+  
+    }
+  
+  });
+
+  
+
+
+
+
+
+
   const maxWirads = 4;
 
   document.querySelector('.setup-similar').classList.remove('hidden');
